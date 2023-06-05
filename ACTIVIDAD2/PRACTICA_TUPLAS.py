024e091fd80062162f25153c05ac3e3fd1679665
AlmaAlmaguer <alma.almaguer@oxxo.com>
2023-06-05 14:59:23 -0600

    ACTIVIDAD2_V2

--- ACTIVIDAD2/PRACTICA_TUPLAS.py
+++ ACTIVIDAD2/PRACTICA_TUPLAS.py
@@ -5,12 +5,12 @@ Utilizar la función "list" para la elaboración de la actividad.
 '''ACTIVIDAD 1 Crea una tupla con una longitud de 5 usando diferentes tipos de datos.'''
 
 tupla = (2,10.5,'hola',True,['i','a'])
-print (type(tupla))
+print(type(tupla))
 
 '''ACTIVIDAD 2 Cambiar dicha tupla a una lista.'''
 
 lista = list(tupla)
-print (type(lista))
+print(type(lista))
 
 
 '''ACTIVIDAD 3 rea un diccionario donde la clave sea del 1 al 5 y los elementos los datos de la lista.'''
@@ -18,6 +18,6 @@ print (type(lista))
 diccionario = {}
 for i in range(5):
     diccionario[i+1] = lista[i]
-print (diccionario)
+print(diccionario)
 
 

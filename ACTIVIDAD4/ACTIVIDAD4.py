b81a1d4f13f56661e706f23a21cd0dd28aa5df9e
AlmaAlmaguer <alma.almaguer@oxxo.com>
2023-06-05 16:15:50 -0600

    Actividad 4

--- ACTIVIDAD4/ACTIVIDAD4.py
+++ ACTIVIDAD4/ACTIVIDAD4.py
@@ -5,13 +5,32 @@
 2.- A un tercio de la edad de mi hermana le disminuyo 15 años para tener la misma edad que yo. Yo tengo 6 años. ¿Qué edad tiene ella? (Escribir la ecuación correspondiente, despejar y calcular la nueva variable "y" de manera manual)
 3.-Determina quién es más grande (variables "x" y "y") utilizando if y else.
 '''
+
+
 ''' AACTIVIDAD1 El doble de mi edad tiene 24 años, ¿cuántos años tengo? (x=?) '''
 
 edad = (24//2)
 x= edad
-print(x)
+print('X= ',x )
+print ('\n')
 
 ''' AACTIVIDAD2 A un tercio de la edad de mi hermana le disminuyo 15 años para tener la misma edad que yo. Yo tengo 6 años. ¿Qué edad tiene ella? (Escribir la ecuación correspondiente, despejar y calcular la nueva variable "y" de manera manual) '''
+nina=6
+nina+=15
 
-print ('(1/3Y) - 15 =6')
-PRINT ('Y/3 + 15=6')
+
+print('(1/3)Y - 15 =6')
+print('(1/3)Y=',nina)
+print('Y =', nina,'x 3')
+nina *=3
+hna = nina
+y = hna
+print ('Y=', y)
+print ('\n')
+
+
+''''ACTIVIDAD 3 Determina quién es más grande (variables "x" y "y") utilizando if y else.'''
+if x >= y:
+    print ('X es Mayor ')
+else:
+    print ('Y es Mayor')

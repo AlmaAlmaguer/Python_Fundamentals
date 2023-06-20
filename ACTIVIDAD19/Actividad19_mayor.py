0ec6c6c5b99a45b843ffc2202762dc4377616dfb
AlmaAlmaguer <alma.almaguer@oxxo.com>
2023-06-19 23:32:28 -0600

    Actividad_mayor

--- ACTIVIDAD19/Actividad19_mayor.py
+++ ACTIVIDAD19/Actividad19_mayor.py
@@ -10,6 +10,7 @@ Es importante imprimir el resultado de cuál es el género que mejor puntaje obt
 facilitar la interpretación de los resultados para el usuario.
 '''
 
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
